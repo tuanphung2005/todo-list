9d3378fcec90ee2582a183ab041f30e9eed76461
nekozap <phunganhtuan@proton.me>
2024-03-30 22:07:18 +0700

    wont do this again

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 document.getElementById('add-button').addEventListener('click', function () {
     const textarea_input = document.getElementById('textarea-input').value;
+
+    if (textarea_input === '') {
+        alert('Please enter a task');
+        return;
+    }
+
     const node = document.createElement('li');
-    
-    // add check box
+
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     node.appendChild(checkbox);
 
-    // wrap the text in a span
     const textSpan = document.createElement('span');
     const textnode = document.createTextNode(textarea_input);
     textSpan.appendChild(textnode);
@@ -17,7 +21,6 @@ document.getElementById('add-button').addEventListener('click', function () {
     console.log(node);
     document.getElementById('textarea-input').value = '';
 
-    // add delete button
     const deleteButton = document.createElement('button');
     deleteButton.innerHTML = 'Delete';
     node.appendChild(deleteButton);
@@ -26,7 +29,6 @@ document.getElementById('add-button').addEventListener('click', function () {
     });
 });
 
-// if checkbox is checked, add strikethrough to the span
 document.getElementById('tasks').addEventListener('change', function (event) {
     if (event.target.type === 'checkbox') {
         event.target.nextSibling.style.textDecoration = event.target.checked ? 'line-through' : 'none';
